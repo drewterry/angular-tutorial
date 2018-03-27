44891e33d15da39a4243aedb235350e6b81934e5
Drew Terry <drew.terry@skookum.com>
2018-03-27 12:24:36 -0400

    downgraded web-api to 0.5.4 and added heroservice mock api

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,22 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+       .pipe(
+        tap(() => this.log(`fetched heroes`)),
+        catchError(this.handleError('getHeroes', []))
+      );
   }
   
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero> (`${this.heroesUrl}/${id}`)
+      .pipe(
+          tap(() => this.log(`fetched hero id=${id}`)),
+          catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
   }
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+  
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  private handleError<T> (operation = 'operation', result?:T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
 
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'; 
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
